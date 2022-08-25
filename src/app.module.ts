2301de247eb59cbd6fc8a67e01312a8086ebe27e
Viacheslav-ua <75425740+Viacheslav-ua@users.noreply.github.com>
2022-08-25 20:45:48 +0300

    #2 Добавлена поддержка mysql, миграции-?

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: './.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        type: config.get<'aurora-mysql'>('TYPEORM_CONNECTION'),
+        host: config.get<string>('TYPEORM_HOST'),
+        username: config.get<string>('TYPEORM_USERNAME'),
+        password: config.get<string>('TYPEORM_PASSWORD'),
+        database: config.get<string>('TYPEORM_DATABASE'),
+        port: Number(config.get<number>('TYPEORM_PORT')),
+        entities: [__dirname + 'dist/**/*.entity{.ts, .js}'],
+        dropSchema: false,
+        synchronize: true,
+        migrationsRun: false,
+        autoLoadEntities: true,
+        logging: true,
+        migrations: ['dist/**/db/migrations/*{.ts,.js}'],
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
