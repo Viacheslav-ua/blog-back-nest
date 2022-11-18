1e30c4d0d9a8da1bb049c391403f2def819797f0
Viacheslav-ua <atph@ukr.net>
2022-11-18 05:32:38 +0200

    cors off

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const config = app.get(ConfigService);
   const port = config.get<number>('API_PORT') || 3000;
   await app.listen(port, () => console.log(`Server started on ${port} port`));
